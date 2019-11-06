9c0c271c4cf15f690cc38bcf205fdfe01c9ea4ca
Aluno <aluno@tub-depe-a0721.tubarao.ifsc.edu.br>
2019-11-06 21:54:28 -0200

    atividades de listas incompletas

---
+++ atividade_listas/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "functions.h"
+
+int main()
+{
+    int escolha;
+
+    Lista* li;
+    Lista* li2;
+
+
+
+    switch (menu()){
+        case 1:
+            printf("ABRIU MENU 1");
+            break;
+        default:
+            printf("deu em nada");
+    }
+
+    return 0;
+}
